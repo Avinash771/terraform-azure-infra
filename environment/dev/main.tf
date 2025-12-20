7723ff0ff63376d86e749cc0eb8a59f0ec2620e5
avinash <avinash122023@gmail.com>
2025-12-20 09:09:43 +0530

    final

--- environment/dev/main.tf
+++ environment/dev/main.tf
@@ -30,12 +30,32 @@ module "nic"{
   vnet_name = var.vnet_name
 }
 
-# module "vm" {
-#   source = "../../modules/linux_virtual_machine"
-#   vm_name = var.vm_name
-#   location = var.location
-#   rg_name = var.rg_name
-  
-# }
+module "nsg" {
+  source = "../../modules/nsg"
+  depends_on = [ module.rg ]
+  rg_name = var.rg_name
+  nsg_rule_name = var.nsg_rule_name
+  priority = var.nsg_priority
+  direction = var.nsg_direction
+  access = var.nsg_access
+  protocol = var.nsg_protocol
+  source_port_range = var.nsg_source_port_range
+  destination_port_range = var.nsg_destination_port_range
+  source_address_prefix = var.nsg_source_address_prefix
+  destination_address_prefix = var.nsg_destination_address_prefix
+}
+
+module "vm" {
+  source                = "../../modules/linux_virtual_machine"
+  depends_on            = [module.nic]
+  vm_name               = var.vm_name
+  location              = var.location
+  rg_name               = var.rg_name
+  resource_group_name   = var.rg_name
+  nic_name              = var.nic_name
+  vm_size               = var.vm_size
+  admin_username        = var.admin_username
+  ssh_public_key_path   = var.ssh_public_key_path
+}
 
 
--- environment/dev/varibales.tf
+++ environment/dev/varibales.tf
@@ -4,4 +4,68 @@ variable "vnet_name" {}
 variable "subnet_name" {}
 # variable "vm_name" {}
 variable "nic_name"{}
+// Add the following variable declarations:
 
+variable "nsg_rule_name" {
+  description = "The name of the NSG rule."
+  type        = string
+}
+
+variable "nsg_priority" {
+  description = "The priority of the NSG rule."
+  type        = number
+}
+
+variable "nsg_direction" {
+  description = "The direction of the NSG rule (Inbound/Outbound)."
+  type        = string
+}
+
+variable "nsg_access" {
+  description = "The access type of the NSG rule (Allow/Deny)."
+  type        = string
+}
+
+variable "nsg_protocol" {
+  description = "The protocol for the NSG rule (Tcp/Udp/All)."
+  type        = string
+}
+
+variable "nsg_source_port_range" {
+  description = "The source port range for the NSG rule."
+  type        = string
+}
+
+variable "nsg_destination_port_range" {
+  description = "The destination port range for the NSG rule."
+  type        = string
+}
+
+variable "nsg_source_address_prefix" {
+  description = "The source address prefix for the NSG rule."
+  type        = string
+}
+
+variable "nsg_destination_address_prefix" {
+  description = "The destination address prefix for the NSG rule."
+  type        = string
+}
+variable "vm_name" {
+  description = "The name of the Virtual Machine"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "The size of the Virtual Machine"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "The admin username for the Virtual Machine"
+  type        = string
+}
+
+variable "ssh_public_key_path" {
+  description = "The path to the SSH public key"
+  type        = string
+}
